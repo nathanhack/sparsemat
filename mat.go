c392cefa7cf8f6635d9fee915ca9d136aa088e62
Nathan Hack <nathan.hack@gmail.com>
2020-09-13 13:03:15 -0400

    initial commit

---
+++ mat.go
@@ -0,0 +1,422 @@
+package mat
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+type Matrix struct {
+	rowValues map[int]map[int]int //hold rowValues for (X,Y)
+	colValues map[int]map[int]int //easy access to (Y,X)
+	rows      int                 // total number rows available to this matrix
+	rowStart  int                 // [rowStart,rowEnd)
+	cols      int                 // total number cols available to this matrix
+	colStart  int                 // [colStart,colEnd)
+
+}
+
+//New creates a new matrix with the specified number of rows and cols.
+// If values is empty, the matrix will be zeroized.
+// If values are not empty it must have rows*cols items.  The values are expected to
+// be 0's or 1's anything else may have unexpected behavior matrix's methods.
+func New(rows, cols int, values ...int) *Matrix {
+	if len(values) != 0 && len(values) != rows*cols {
+		panic(fmt.Sprintf("matrix data length (%v) to size mismatch expected %v", len(values), rows*cols))
+	}
+
+	mat := Matrix{
+		rowValues: map[int]map[int]int{},
+		colValues: map[int]map[int]int{},
+		rows:      rows,
+		rowStart:  0,
+		cols:      cols,
+		colStart:  0,
+	}
+
+	if len(values) > 0 {
+		for i := 0; i < rows; i++ {
+			for j := 0; j < cols; j++ {
+				index := i*cols + j
+				mat.set(i, j, values[index])
+			}
+		}
+	}
+
+	return &mat
+}
+
+//Identity create an identity matrix (one's on the diagonal).
+func Identity(size int) *Matrix {
+	mat := Matrix{
+		rowValues: map[int]map[int]int{},
+		colValues: map[int]map[int]int{},
+		rows:      size,
+		rowStart:  0,
+		cols:      size,
+		colStart:  0,
+	}
+
+	for i := 0; i < size; i++ {
+		mat.set(i, i, 1)
+	}
+
+	return &mat
+}
+
+//Copy will create a NEW matrix that will have all the same values as m.
+func Copy(m *Matrix) *Matrix {
+	mat := Matrix{
+		rowValues: make(map[int]map[int]int),
+		colValues: make(map[int]map[int]int),
+		rows:      m.rows,
+		rowStart:  0,
+		cols:      m.cols,
+		colStart:  0,
+	}
+
+	for i := 0; i < m.rows; i++ {
+		for j := 0; j < m.cols; j++ {
+			mat.set(i, j, m.At(i, j))
+		}
+	}
+
+	return &mat
+}
+
+//Slice creates a slice of the matrix.  The slice will be connected to the original matrix, changes work to one
+// causes changes in the other
+func (mat *Matrix) Slice(i, j, rows, cols int) *Matrix {
+	if rows <= 0 || cols <= 0 {
+		panic("slice rows and cols must >= 1")
+	}
+
+	mat.checkRowBounds(i)
+	mat.checkColBounds(j)
+	r := i + mat.rowStart
+	c := j + mat.colStart
+
+	if r+rows-1 > mat.rows || c+cols-1 > mat.cols {
+		panic("slice rows and cols must be in bounds of matrix")
+	}
+	mat.checkRowBounds(i + rows - 1)
+	mat.checkColBounds(j + cols - 1)
+
+	return &Matrix{
+		rowValues: mat.rowValues,
+		rows:      rows,
+		rowStart:  r,
+		colValues: mat.colValues,
+		cols:      cols,
+		colStart:  c,
+	}
+}
+
+func (mat *Matrix) checkRowBounds(i int) {
+	if i < 0 || i >= mat.rows {
+		panic(fmt.Sprintf("%v out of range: [0-%v]", i, mat.rows-1))
+	}
+
+}
+
+func (mat *Matrix) checkColBounds(j int) {
+	if j < 0 || j >= mat.cols {
+		panic(fmt.Sprintf("%v out of range: [0-%v]", j, mat.cols-1))
+	}
+}
+
+//Dim returns the dimensions of the matrix.
+func (mat *Matrix) Dim() (int, int) {
+	return mat.rows, mat.cols
+}
+
+//At returns the value at row index i and column index j.
+func (mat *Matrix) At(i, j int) int {
+	mat.checkRowBounds(i)
+	mat.checkColBounds(j)
+	r := i + mat.rowStart
+	c := j + mat.colStart
+
+	return mat.at(r, c)
+}
+
+func (mat *Matrix) at(r, c int) int {
+	ys, ok := mat.rowValues[r]
+	if !ok {
+		return 0
+	}
+	v, ok := ys[c]
+	if !ok {
+		return 0
+	}
+	return v
+}
+
+//Set sets the value at row index i and column index j to value.
+func (mat *Matrix) Set(i, j, value int) {
+	mat.checkRowBounds(i)
+	mat.checkColBounds(j)
+	r := i + mat.rowStart
+	c := j + mat.colStart
+
+	mat.set(r, c, value)
+}
+
+func (mat *Matrix) set(r, c, value int) {
+	if value == 0 {
+		ys, ok := mat.rowValues[r]
+		if !ok {
+			return
+		}
+
+		_, ok = ys[c]
+		if !ok {
+			return
+		}
+
+		delete(ys, c)
+		if len(mat.rowValues[r]) == 0 {
+			delete(mat.rowValues, r)
+		}
+
+		delete(mat.colValues[c], r)
+		if len(mat.colValues[c]) == 0 {
+			delete(mat.colValues, c)
+		}
+
+		return
+	}
+
+	ys, ok := mat.rowValues[r]
+	if !ok {
+		ys = make(map[int]int)
+		mat.rowValues[r] = ys
+	}
+	ys[c] = value
+
+	xs, ok := mat.colValues[c]
+	if !ok {
+		xs = make(map[int]int)
+		mat.colValues[c] = xs
+	}
+	xs[r] = value
+}
+
+//T returns a matrix that is the transpose of the underlying matrix. Note the transpose
+// is connected to matrix it is a transpose of, and changes made to one affect the other.
+func (mat *Matrix) T() *Matrix {
+	return &Matrix{
+		rowValues: mat.colValues,
+		rows:      mat.cols,
+		rowStart:  mat.colStart,
+		colValues: mat.rowValues,
+		cols:      mat.rows,
+		colStart:  mat.rowStart,
+	}
+}
+
+//Zeroize take the current matrix and zeroizes it sets all values to 0.
+func (mat *Matrix) Zeroize() {
+	for r, cs := range mat.rowValues {
+		i := r - mat.rowStart
+		if i >= 0 && i < mat.rows {
+			for c, v := range cs {
+				if v == 0 {
+					continue
+				}
+				j := c - mat.colStart
+				if j >= 0 && j < mat.cols {
+					mat.set(r, c, 0)
+				}
+			}
+		}
+	}
+}
+
+//Mul multiplies two matrices and stores the values in this matrix.
+func (mat *Matrix) Mul(a, b *Matrix) {
+	if a == nil || b == nil {
+		panic("multiply input was found to be nil")
+	}
+	if mat == a || mat == b {
+		panic("multiply self assignment not allowed")
+	}
+	if a.cols != b.rows {
+		panic(fmt.Sprintf("multiply shape misalignment can't multiple (%v,%v)x(%v,%v)", a.rows, a.cols, b.rows, b.cols))
+	}
+	mRows, mCols := mat.Dim()
+	aRows, _ := a.Dim()
+	_, bCols := b.Dim()
+
+	if mRows != aRows || mCols != bCols {
+		panic(fmt.Sprintf("mat shape (%v,%v) does not match expected (%v,%v)", mat.rows, mat.cols, a.rows, b.cols))
+	}
+
+	//first we need to clear mat
+	mat.Zeroize()
+
+	for r, cs := range a.rowValues {
+		i := r - a.rowStart
+
+		for c, rs := range b.colValues {
+			j := c - b.colStart
+			value := 0
+			for ics, v1 := range cs {
+				ci := ics - a.colStart
+
+				v2, ok := rs[ci+b.rowStart]
+				if ok {
+					value += v1 * v2
+				}
+			}
+			value = value % 2
+
+			mat.Set(i, j, value)
+		}
+	}
+}
+
+//Add stores the addition of a and b in this matrix.
+func (mat *Matrix) Add(a, b *Matrix) {
+	if a == nil || b == nil {
+		panic("addition input was found to be nil")
+	}
+	if mat == a || mat == b {
+		panic("addition self assignment not allowed")
+	}
+
+	if a.rows != b.rows || a.cols != b.cols {
+		panic(fmt.Sprintf("addition input mat shapes do not match a=(%v,%v) b=(%v,%v)", a.rows, a.cols, b.rows, b.cols))
+	}
+	if mat.rows != a.rows || mat.cols != a.cols {
+		panic(fmt.Sprintf("mat shape (%v,%v) does not match expected (%v,%v)", mat.rows, mat.cols, a.rows, a.cols))
+	}
+
+	//first we need to clear mat
+	mat.SetMatrix(a)
+
+	for r, cs := range b.rowValues {
+		i := r - b.rowStart
+		mr := i + mat.rowStart
+		for c, v := range cs {
+			j := c - b.colStart
+			mc := j + mat.colStart
+			mat.set(mr, mc, (mat.at(mr, mc)+v)%2)
+		}
+	}
+}
+
+//Column returns a map containing the non zero row indices as the keys and it's associated values.
+func (mat *Matrix) Column(j int) map[int]int {
+	mat.checkColBounds(j)
+	col := j + mat.colStart
+	result := make(map[int]int)
+
+	for r, v := range mat.colValues[col] {
+		if mat.rowStart <= r && r < mat.rowStart+mat.rows {
+			result[r-mat.rowStart] = v
+		}
+	}
+	return result
+}
+
+//Row returns a map containing the non zero column indices as the keys and it's associated values.
+func (mat *Matrix) Row(i int) map[int]int {
+	mat.checkRowBounds(i)
+	row := i + mat.rowStart
+	result := make(map[int]int)
+
+	for c, v := range mat.rowValues[row] {
+		if mat.colStart <= c && c < mat.colStart+mat.cols {
+			result[c-mat.rowStart] = v
+		}
+	}
+	return result
+}
+
+//Equals return true if the m matrix has the same shape and values as this matrix.
+func (mat *Matrix) Equals(m *Matrix) bool {
+	if mat == m {
+		return true
+	}
+
+	if mat == nil || m == nil {
+		return false
+	}
+
+	if mat.rows != m.rows || mat.cols != m.cols {
+		return false
+	}
+
+	for i := 0; i < mat.rows; i++ {
+		r := i + mat.rowStart
+		cs, ok1 := mat.rowValues[r]
+		ar := i + m.rowStart
+		acs, ok2 := m.rowValues[ar]
+
+		if !ok1 && !ok2 {
+			continue
+		}
+
+		for j := 0; j < mat.cols; j++ {
+			c := j + mat.colStart
+			v1, ok1 := cs[c]
+			ac := j + m.colStart
+			v2, ok2 := acs[ac]
+
+			if !ok1 && !ok2 {
+				continue
+			}
+
+			if (!ok1 && ok2) || (ok1 && !ok2) {
+				return false
+			}
+
+			if v1 != v2 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+//String returns a string representation of this matrix.
+func (mat Matrix) String() string {
+	buff := &strings.Builder{}
+	table := tablewriter.NewWriter(buff)
+
+	table.SetBorder(false)
+	table.SetColumnSeparator("")
+	table.SetRowSeparator("")
+	table.SetHeaderLine(false)
+
+	for i := 0; i < mat.rows; i++ {
+		row := make([]string, mat.cols)
+		for j := 0; j < mat.cols; j++ {
+			row[j] = fmt.Sprint(mat.At(i, j))
+		}
+		table.Append(row)
+	}
+
+	table.Render()
+	return buff.String()
+}
+
+func (mat *Matrix) SetMatrix(a *Matrix) {
+	if mat.rows != a.rows || mat.cols != a.cols {
+		panic(fmt.Sprintf("set matrix must have the same shape, expected (%v,%v) but a=(%v,%v)", mat.rows, mat.cols, a.rows, a.cols))
+	}
+
+	mat.Zeroize()
+
+	for r, cs := range a.rowValues {
+		i := r - a.rowStart
+		mr := i + mat.rowStart
+		for c, v := range cs {
+			j := c - a.colStart
+			mc := j + mat.colStart
+			mat.set(mr, mc, v)
+		}
+	}
+}
---
+++ mat_test.go
@@ -0,0 +1,333 @@
+package mat
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		rows, cols int
+		data       []int
+		expected   [][]int
+	}{
+		{1, 1, []int{1}, [][]int{{1}}},
+		{2, 2, []int{1, 0, 0, 1}, [][]int{{1, 0}, {0, 1}}},
+		{2, 2, []int{}, [][]int{{0, 0}, {0, 0}}},
+		{2, 2, nil, [][]int{{0, 0}, {0, 0}}},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			var m *Matrix
+			if test.data != nil {
+				m = New(test.rows, test.cols, test.data...)
+			} else {
+				m = New(test.rows, test.cols)
+			}
+
+			for i := 0; i < len(test.expected); i++ {
+				for j := 0; j < len(test.expected[i]); j++ {
+					expected := test.expected[i][j]
+					actual := m.At(i, j)
+					if actual != expected {
+						t.Fatalf("expected %v but found %v", expected, actual)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestCopy(t *testing.T) {
+	tests := []struct {
+		rows, cols int
+		data       []int
+		expected   [][]int
+	}{
+		{1, 1, []int{1}, [][]int{{1}}},
+		{2, 2, []int{1, 0, 0, 1}, [][]int{{1, 0}, {0, 1}}},
+		{2, 2, []int{}, [][]int{{0, 0}, {0, 0}}},
+		{2, 2, nil, [][]int{{0, 0}, {0, 0}}},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			var m1 *Matrix
+			if test.data != nil {
+				m1 = New(test.rows, test.cols, test.data...)
+			} else {
+				m1 = New(test.rows, test.cols)
+			}
+
+			m := Copy(m1)
+
+			for i := 0; i < len(test.expected); i++ {
+				for j := 0; j < len(test.expected[i]); j++ {
+					expected := test.expected[i][j]
+					actual := m.At(i, j)
+					if actual != expected {
+						t.Fatalf("expected %v but found %v", expected, actual)
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestMatrix_Dim(t *testing.T) {
+	tests := []struct {
+		m            *Matrix
+		expectedRows int
+		expectedCols int
+	}{
+		{New(5, 5), 5, 5},
+		{New(5, 5).Slice(1, 1, 4, 4), 4, 4},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			rows, cols := test.m.Dim()
+
+			if rows != test.expectedRows {
+				t.Fatalf("expected %v but found %v", test.expectedRows, rows)
+			}
+			if cols != test.expectedCols {
+				t.Fatalf("expected %v but found %v", test.expectedCols, cols)
+			}
+		})
+	}
+}
+
+func TestMatrix_Slice(t *testing.T) {
+	tests := []struct {
+		sliced   *Matrix
+		expected *Matrix
+	}{
+		{New(2, 2, 1, 0, 0, 1).Slice(0, 0, 2, 1), New(2, 1, 1, 0)},
+		{Identity(8).Slice(3, 0, 4, 4), New(4, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)},
+		{Identity(8).Slice(3, 0, 4, 4).T(), New(4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0)},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !test.sliced.Equals(test.expected) {
+				t.Fatalf("expected equality between %v and %v", test.sliced, test.expected)
+			}
+
+		})
+	}
+}
+
+func TestMatrix_Slice2(t *testing.T) {
+
+	orignal := Identity(5)
+
+	slice := orignal.Slice(1, 1, 3, 3).T()
+
+	slice.Zeroize()
+
+	slice.Set(1, 0, 1)
+	slice.Set(2, 0, 1)
+
+	expectedSlice := New(3, 3, 0, 0, 0, 1, 0, 0, 1, 0, 0)
+	expectedOriginal := New(5, 5, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1)
+
+	if !slice.Equals(expectedSlice) {
+		t.Fatalf("expcted \n%v\n but found \n%v\n", expectedSlice, slice)
+	}
+	if !orignal.Equals(expectedOriginal) {
+		t.Fatalf("expcted \n%v\n but found \n%v\n", expectedOriginal, orignal)
+	}
+}
+
+func TestMatrix_Equals(t *testing.T) {
+	tests := []struct {
+		input1, input2 *Matrix
+		expected       bool
+	}{
+		{Identity(3), Identity(3), true},
+		{Identity(3).T(), Identity(3), true},
+		{Identity(4), Identity(3), false},
+		{Identity(4), nil, false},
+		{nil, Identity(4), false},
+		{nil, nil, true},
+		{New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0).T(), New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0), false},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := test.input1.Equals(test.input2)
+			if actual != test.expected {
+				t.Fatalf("expected %v but found %v", test.expected, actual)
+			}
+		})
+	}
+}
+
+func TestIdentity(t *testing.T) {
+	tests := []struct {
+		ident    *Matrix
+		expected *Matrix
+	}{
+		{Identity(3), New(3, 3, 1, 0, 0, 0, 1, 0, 0, 0, 1)},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !test.ident.Equals(test.expected) {
+				t.Fatalf("expected equality")
+			}
+		})
+	}
+}
+
+func TestMatrix_At(t *testing.T) {
+	tests := []struct {
+		input    *Matrix
+		i, j     int
+		expected int
+	}{
+		{Identity(3), 0, 0, 1},
+		{Identity(3), 0, 1, 0},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := test.input.At(test.i, test.j)
+			if actual != test.expected {
+				t.Fatalf("expected %v at (%v,%v) but found %v", test.expected, test.i, test.j, actual)
+			}
+		})
+	}
+}
+
+func TestMatrix_Mul(t *testing.T) {
+	tests := []struct {
+		m1, m2, result, expected *Matrix
+	}{
+		{New(1, 4, 1, 0, 1, 0), New(4, 1, 1, 0, 1, 0), New(1, 1), New(1, 1, 0)},
+		{New(1, 4, 1, 0, 1, 0), New(4, 1, 1, 0, 0, 0), New(1, 1), New(1, 1, 1)},
+		{New(1, 4, 1, 1, 1, 1), New(4, 1, 1, 1, 1, 0), New(1, 1), New(1, 1, 1)},
+		{Identity(3), Identity(3), New(3, 3), Identity(3)},
+		{Identity(3), New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0), New(3, 3), New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0)},
+		{New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0), Identity(3), New(3, 3), New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0)},
+		{New(4, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1).T(), Identity(4), New(3, 4), New(4, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1).T()},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			test.result.Mul(test.m1, test.m2)
+			if !test.result.Equals(test.expected) {
+				t.Fatalf("expected equality")
+			}
+		})
+	}
+}
+
+func TestMatrix_Zeroize(t *testing.T) {
+	tests := []struct {
+		original *Matrix
+		expected *Matrix
+	}{
+		{Identity(3), New(3, 3)},
+		{New(3, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1), New(3, 3)},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			test.original.Zeroize()
+			if !test.original.Equals(test.expected) {
+				t.Fatalf("expcted \n%v\n but found \n%v\n", test.expected, test.original)
+			}
+		})
+	}
+}
+
+func TestMatrix_T(t *testing.T) {
+	tests := []struct {
+		original *Matrix
+		expected *Matrix
+	}{
+		{New(3, 3, 0, 1, 1, 0, 1, 1, 0, 0, 0), New(3, 3, 0, 0, 0, 1, 1, 0, 1, 1, 0)},
+		{New(4, 2, 0, 1, 0, 0, 0, 0, 1, 0), New(2, 4, 0, 0, 0, 1, 1, 0, 0, 0)},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			if !test.original.T().Equals(test.expected) {
+				t.Fatalf("expcted \n%v\n but found \n%v\n", test.expected, test.original.T())
+			}
+		})
+	}
+}
+
+func TestMatrix_Add(t *testing.T) {
+	tests := []struct {
+		a, b, result *Matrix
+		expected     *Matrix
+	}{
+		{Identity(3), Identity(3), New(3, 3), New(3, 3)},
+		{Identity(3), New(3, 3), New(3, 3), Identity(3)},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			test.result.Add(test.a, test.b)
+			if !test.result.Equals(test.expected) {
+				t.Fatalf("expcted \n%v\n but found \n%v\n", test.expected, test.result)
+			}
+		})
+	}
+}
+
+func TestMatrix_Column(t *testing.T) {
+	tests := []struct {
+		m        *Matrix
+		j        int //column
+		expected map[int]int
+	}{
+		{Identity(3), 1, map[int]int{1: 1}},
+		{Identity(3), 0, map[int]int{0: 1}},
+		{New(4, 4, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0).Slice(1, 1, 2, 2).T(), 0, map[int]int{1: 1}},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := test.m.Column(test.j)
+
+			if len(actual) != len(test.expected) {
+				t.Fatalf("expected %v values found %v: %v", len(test.expected), len(actual), actual)
+			}
+
+			for row, v1 := range test.expected {
+				v2, ok := actual[row]
+				if !ok {
+					t.Fatalf("expected to find row %v", row)
+				}
+				if v1 != v2 {
+					t.Fatalf("expected value at row %v to be %v but found %v", row, v1, v2)
+				}
+			}
+		})
+	}
+}
+
+func TestMatrix_Row(t *testing.T) {
+	tests := []struct {
+		m        *Matrix
+		i        int //row index
+		expected map[int]int
+	}{
+		{Identity(3), 1, map[int]int{1: 1}},
+		{Identity(3), 0, map[int]int{0: 1}},
+		{New(4, 4, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0).Slice(1, 1, 2, 2).T(), 1, map[int]int{0: 1}},
+	}
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			actual := test.m.Row(test.i)
+
+			if len(actual) != len(test.expected) {
+				t.Fatalf("expected %v values found %v: %v", len(test.expected), len(actual), actual)
+			}
+
+			for col, v1 := range test.expected {
+				v2, ok := actual[col]
+				if !ok {
+					t.Fatalf("expected to find col %v", col)
+				}
+				if v1 != v2 {
+					t.Fatalf("expected value at col %v to be %v but found %v", col, v1, v2)
+				}
+			}
+		})
+	}
+}
